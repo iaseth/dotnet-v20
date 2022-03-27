55add0be46e0db0cad5c95442021036f9272d6c3
Ankur Seth <iankurseth@gmail.com>
2022-03-27 21:09:02 +0530

    Added printTeams(), printGrounds() and printPlayers() on CodesJson

--- CodesJson.cs
+++ CodesJson.cs
@@ -8,32 +8,62 @@ namespace iplr_dotnet
 	{
 		public int id;
 		public string fn, sn, abb, name, color;
+
+		public void print()
+		{
+			Console.WriteLine("Team: " + fn + " (" + abb + ")");
+		}
 	}
 
 	class GroundJson
 	{
 		public int id;
 		public string fn, sn, city, country, path;
+
+		public void print()
+		{
+			Console.WriteLine("Ground: " + fn + " (" + city + ")");
+		}
 	}
 
 	class PlayerJson
 	{
 		public int id;
 		public string fn, sn, country, path;
+
+		public void print()
+		{
+			Console.WriteLine("Player: " + fn + " (" + country + ")");
+		}
 	}
 
 	class CodesJson
-    {
-        public List<TeamJson> teams;
-        public List<GroundJson> grounds;
-        public List<PlayerJson> players;
+	{
+		public List<TeamJson> teams;
+		public List<GroundJson> grounds;
+		public List<PlayerJson> players;
 
-        public void print()
-        {
-            Console.WriteLine("CodesJson object.");
-            Console.WriteLine("\t---- " + teams.Count + " teams");
-            Console.WriteLine("\t---- " + grounds.Count + " grounds");
-            Console.WriteLine("\t---- " + players.Count + " players");
-        }
-    }
+		public void printTeams()
+		{
+			foreach (var team in teams) team.print();
+		}
+
+		public void printGrounds()
+		{
+			foreach (var ground in grounds) ground.print();
+		}
+
+		public void printPlayers()
+		{
+			foreach (var player in players) player.print();
+		}
+
+		public void print()
+		{
+			Console.WriteLine("CodesJson object.");
+			Console.WriteLine("\t---- " + teams.Count + " teams");
+			Console.WriteLine("\t---- " + grounds.Count + " grounds");
+			Console.WriteLine("\t---- " + players.Count + " players");
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ using Newtonsoft.Json;
 
 namespace iplr_dotnet
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            using (StreamReader file = File.OpenText(@"data/codes.json"))
-            {
-                JsonSerializer js = new JsonSerializer();
-                CodesJson cj = (CodesJson)js.Deserialize(file, typeof(CodesJson));
-                cj.print();
-            }
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (StreamReader file = File.OpenText(@"data/codes.json"))
+			{
+				JsonSerializer js = new JsonSerializer();
+				CodesJson cj = (CodesJson)js.Deserialize(file, typeof(CodesJson));
+				cj.print();
+			}
+		}
+	}
 }
