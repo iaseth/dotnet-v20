bb1e0ce10633a58cebc0d5666e0bf52e48f3abd3
Ankur Seth <iankurseth@gmail.com>
2022-03-28 08:55:04 +0530

    Correctly initing MatchMetaJson

--- src/BundleJson.cs
+++ src/BundleJson.cs
@@ -6,11 +6,37 @@ namespace IplrDotnet
 	// disables warning that field is never assigned to
 	#pragma warning disable 0649
 
+	class TeamInningJson {
+		public int runs;
+	}
+
+	class SquadJson {
+		public int team, captain, wk;
+		public List<int> players;
+	}
+
+	class MatchMetaJson {
+		public int ground, number;
+		public string outcome, state, year;
+		public List<int> order;
+
+		public void print() {
+			Console.WriteLine(" Ground: " + ground);
+			Console.WriteLine(" Number: " + number);
+			Console.WriteLine("  State: " + state);
+			Console.WriteLine("Outcome: " + outcome);
+			Console.WriteLine("   Year: " + year);
+			Console.WriteLine("  Order: " + order);
+		}
+	}
+
 	class MatchJson {
-		//
+		public MatchMetaJson meta;
+		public List<SquadJson> teams;
 
 		public void print() {
 			Console.WriteLine("Match object {}");
+			meta.print();
 		}
 	}
 
