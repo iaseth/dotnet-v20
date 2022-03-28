66d767810f53a906b197d291e4b4a87b41512689
Ankur Seth <iankurseth@gmail.com>
2022-03-28 13:02:08 +0530

    Added basic Ground class and League.printGrounds()

---
+++ src/Ground.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace V20 {
+	class Ground {
+		League league;
+		GroundJson jo;
+		int id;
+		string fn, sn, path, city, country;
+
+		public Ground(League league, GroundJson jo) {
+			this.league = league;
+			this.jo = jo;
+			id = jo.id;
+			fn = jo.fn; sn = jo.sn; path = jo.path;
+			city = jo.city; country = jo.country;
+		}
+
+		public void print() {
+			Console.WriteLine("Ground: " + sn + " (" + city + ", " + country + ")");
+		}
+	}
+}
--- src/League.cs
+++ src/League.cs
@@ -7,10 +7,11 @@ namespace V20 {
 		BundleJson bj;
 
 		List<Team> teams;
+		List<Ground> grounds;
 
 		public League(CodesJson cj) {
 			this.cj = cj;
-			setupTeams();
+			setupTeams(); setupGrounds(); setupPlayers();
 		}
 
 		public void setupTeams() {
@@ -21,18 +22,38 @@ namespace V20 {
 			}
 		}
 
+		public void setupGrounds() {
+			grounds = new List<Ground>();
+			foreach (GroundJson jo in cj.grounds) {
+				Ground ground = new Ground(this, jo);
+				grounds.Add(ground);
+			}
+		}
+
+		public void setupPlayers() {
+			//
+		}
+
+
 		public void loadBundle(BundleJson bj) {
 			this.bj = bj;
 		}
 
 		public void doStuff() {
-			printTeams();
+			// printTeams();
+			printGrounds();
 		}
 
 
 		public void printTeams() {
 			foreach (Team team in teams) {
 				team.print();
+			}
+		}
+
+		public void printGrounds() {
+			foreach (Ground ground in grounds) {
+				ground.print();
 			}
 		}
 
